4cb2c42d0f918fbfe95ed9ddc2cf422f1dee334c
Colton Bristow <56052449+ColtonDevAcc@users.noreply.github.com>
2022-04-28 20:10:57 -0500

    .ignore

---
+++ GO/source/myapp/handlers/handlers.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/tsawler/celeritas"
+)
+
+type Handlers struct {
+	App *celeritas.Celeritas
+}
+
+func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
+	err := h.App.Render.Page(w, r, "home", nil, nil)
+	if err != nil {
+		h.App.ErrorLog.Println("error rendering:", err)
+	}
+}
+
+func (h *Handlers) GoPage(w http.ResponseWriter, r *http.Request) {
+	err := h.App.Render.GoPage(w, r, "home", nil)
+	if err != nil {
+		h.App.ErrorLog.Println("error rendering:", err)
+	}
+}
+
+func (h *Handlers) JetPage(w http.ResponseWriter, r *http.Request) {
+	err := h.App.Render.JetPage(w, r, "jet-template", nil, nil)
+	if err != nil {
+		h.App.ErrorLog.Println("error rendering:", err)
+	}
+}
